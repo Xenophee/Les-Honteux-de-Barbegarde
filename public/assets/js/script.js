a9929d86c5adad0c9f37109518b7799c78db197c
Perrine <perrine.morsch@gmail.com>
2023-05-04 22:57:45 +0200

    mise en place des images du jeu en js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,33 @@
-const rules = document.getElementById('rules');
-const openRules = document.getElementById('openRules');
-const closeRules = document.getElementById('closeRules');
 
 
 
+// OUVERTURE DES REGLES DU JEU
 openRules.addEventListener('click', () => {
     rules.showModal();
-})
+});
 
+// FERMETURE DES REGLES DU JEU
 closeRules.addEventListener('click', () => {
     rules.close();
-})
+});
+
+
+// AFFICHE L'INPUT POUR LE MOT DE PASSE
+cheat.addEventListener('click', () => {
+    passwordSection.classList.toggle('hide');
+});
+
+// FERME L'ONGLET DE TRICHE
+closeCheat.addEventListener('click', () => {
+    choosingCheat.classList.add('hide');
+    cheatingEnter.classList.remove('hide');
+});
+
+// AFFICHE LA SECTION DE TRICHE
+password.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && password.value == 'honteux') {
+        cheatingEnter.classList.add('hide');
+        passwordSection.classList.add('hide');
+        choosingCheat.classList.remove('hide');
+    }
+});
